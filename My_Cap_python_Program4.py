529b88e2315b36b4cf7790fba96b1257f72efe47
1503AbhinavGarg <147425284+1503AbhinavGarg@users.noreply.github.com>
2023-10-26 19:42:14 +0530

    Add files via upload

---
+++ My_Cap_python_Program4.py
@@ -0,0 +1,16 @@
+List1=[12,-7,5,64,-14]
+List2=[12,14,-95,3]
+
+print("Input:List1=[12,-7,5,64,-14]")
+print("Output:")
+for i in List1:
+    if i<=0:
+        continue
+    print(i)
+
+print("\nInput:List2=[12,14,-95,3]")
+print("Output:")
+for i in List2:
+    if i<=0:
+        continue
+    print(i)
